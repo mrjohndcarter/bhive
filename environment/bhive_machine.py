5702e2eafb2ea0e223eaa44a5c905866da99b844
John Carter <mrjohndcarter@gmail.com>
2016-09-10 16:54:27 -0400

    Very basic synthesis happening (only semantic)

--- environment/bhive_machine.py
+++ environment/bhive_machine.py
@@ -4,20 +4,120 @@ Container for a B-Machine
 import os
 
 
+# TODO: Don't support machine parameters
 class BHiveMachine(object):
-    MACHINE_EXTENSION = "mch"
+    class Parameter(object):
+        def __init__(self, name):
+            self.name = name
+            self.type = None
+            self.constraint_expression = None
+
+        def param_type(self):
+            return ' : '.join([self.name, self.type])
+
+        def constraint(self):
+            return self.constraint_expression
+
+    class Set(object):
+        def __init__(self, name):
+            self.name = name
+            self.enumeration = None
+
+        def set(self):
+            if not self.enumeration:
+                return self.name
+            else:
+                return ' '.join([self.name, '=', self.enumeration])
+
+    class Assignable(object):
+        def __init__(self, name):
+            self.name = name
+            self.type = None
+            self.assignment_expression = None
+
+    class Constant(Assignable):
+        def __init__(self, name):
+            super(BHiveMachine.Constant, self).__init__(name)
+
+        def property(self):
+            return ' '.join([self.name, ':', self.type, '&', self.assignment_expression])
+
+    class Variable(Assignable):
+        def __init__(self, name):
+            super(BHiveMachine.Variable, self).__init__(name)
+
+        def invariant(self):
+            return ' '.join([self.name, ':', self.type])
+
+        def initialisation(self):
+            return ' '.join([self.name, ':=', self.assignment_expression])
+
+    MACHINE_EXTENSION = 'mch'
+    WHITE_SPACE_SEP_STRING = '\t\t\t\t'
+    LINE_SEP_STRING = '\n\n'
 
     def __init__(self, name):
         self.name = name
         self.operations = {}
+        self.constants = {}
+        self.variables = {}
+        self.sets = {}
+        self.parameters = []
 
     def __str__(self):
-        build_string = "Machine: "
-        build_string += self.name
-        build_string += "\n"
-        for operation in self.operations.values():
-            build_string += "\n" + str(operation)
+        build_string = self.WHITE_SPACE_SEP_STRING.join(['MACHINE', self.name])
+
+        if len(self.parameters) > 0:
+            build_string += ''.join(['(', ', '.join([p.name for p in self.parameters]), ')'])
+            build_string += self.LINE_SEP_STRING
+            build_string += 'CONSTRAINTS'
+            build_string += self.WHITE_SPACE_SEP_STRING + ' & '.join(
+                [(' & '.join([c.param_type() for c in self.parameters])),
+                 (' & '.join([c.constraint_expression for c in self.parameters]))])
+
+        build_string += self.LINE_SEP_STRING
+        build_string += 'SETS'
+        build_string += self.WHITE_SPACE_SEP_STRING + '; '.join([s.set() for s in self.sets.values()])
+        build_string += self.LINE_SEP_STRING
+        build_string += 'CONSTANTS'
+        build_string += self.WHITE_SPACE_SEP_STRING + ' & '.join([c.name for c in self.constants.values()])
+        build_string += self.LINE_SEP_STRING
+        build_string += 'PROPERTIES'
+        build_string += self.WHITE_SPACE_SEP_STRING + ' & '.join([c.property() for c in self.constants.values()])
+        build_string += self.LINE_SEP_STRING
+        build_string += 'VARIABLES'
+        build_string += self.WHITE_SPACE_SEP_STRING + ', '.join([v.name for v in self.variables.values()])
+        build_string += self.LINE_SEP_STRING
+        build_string += 'INVARIANT'
+        build_string += self.WHITE_SPACE_SEP_STRING + ', '.join([v.invariant() for v in self.variables.values()])
+        build_string += self.LINE_SEP_STRING
+        build_string += 'INITIALISATION'
+        build_string += self.WHITE_SPACE_SEP_STRING + ', '.join([v.initialisation() for v in self.variables.values()])
+        build_string += self.LINE_SEP_STRING
+
+        build_string += 'OPERATIONS'
+        build_string += self.LINE_SEP_STRING
+
+        # for operation in self.operations.values():
+        # build_string += self.LINE_SEP_STRING + str(operation)
+
+        build_string += (';' + self.WHITE_SPACE_SEP_STRING).join([str(op) for op in self.operations.values()])
+
+        build_string += '\nEND'
         return build_string
+
+    def add_parameter(self, parameter):
+        # TODO check for same name
+        self.parameters.append(parameter)
+
+    def add_set(self, set):
+        self.sets[set.name] = set
+
+    def add_variable(self, variable):
+        self.variables[variable.name] = variable
+
+    def add_constant(self, constant):
+        self.constants[constant.name] = constant
 
     def add_operation(self, operation):
         self.operations[operation.name] = operation
--- environment/bhive_operation.py
+++ environment/bhive_operation.py
@@ -7,9 +7,43 @@ from mock import Mock
 
 
 class BHiveOperation(object):
+    class Precondition(object):
+        def __init__(self, expression):
+            self.expression = expression
+
+        def __str__(self):
+            return ''.join(['PRE ', self.expression])
+
+    class Assignment(object):
+        def __init__(self, expression):
+            self.expression = expression
+
+        def __str__(self):
+            return ''.join(['THEN ', self.expression])
+
+    class OperationParameter(object):
+        def __init__(self, name, type):
+            self.name = name
+            self.type = type
+
+        @staticmethod
+        def format_parameters_list(parameters_list):
+            return '' if len(parameters_list) == 0 else ('(' + ','.join([p.name for p in parameters_list]) + ')')
+
     def __init__(self, name):
         self.name = name
         self.precondition = None
+        self.assignment = None
+        self.parameters_list = []
+
+    def __str__(self):
+        build_string = ' '.join(
+            [self.name, BHiveOperation.OperationParameter.format_parameters_list(self.parameters_list)])
+        build_string += '='
+        build_string += '\nPRE ' + str(self.precondition)
+        build_string += '\nTHEN ' + str(self.assignment)
+        build_string += '\nEND'
+        return build_string
 
     @staticmethod
     def get_operation_name_from_scenario(scenario):
@@ -34,6 +68,12 @@ class TestBHiveOperation(unittest.TestCase):
         assert BHiveOperation.get_operation_name_from_scenario(scenario_mock) == 'Start_Elevator'
         scenario_mock.name = '   StopEscalator'
         assert BHiveOperation.get_operation_name_from_scenario(scenario_mock) == 'StopEscalator'
+
+    def test_formatting_operation_parameter_string(self):
+        assert BHiveOperation.OperationParameter.format_parameters_list([]) == ''
+        assert BHiveOperation.OperationParameter.format_parameters_list(
+            [BHiveOperation.OperationParameter('a', 'A'),
+             BHiveOperation.OperationParameter('b', 'B')]) == '(a,b)'
 
 
 if __name__ == '__main__':
