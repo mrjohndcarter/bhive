5252e8e6420bd743910ff6fc7ad01d2b0a5f7829
John Carter <mrjohndcarter@gmail.com>
2016-09-25 21:50:00 -0400

    Removed unused type file.

--- typing/type.py
+++
@@ -1,9 +0,0 @@
-class Type(object):
-    def __init__(self, name, parse_function):
-        # TODO: call up and register type
-        pass
-
-    def __str__(self):
-        pass
-
-# TODO: define B types
